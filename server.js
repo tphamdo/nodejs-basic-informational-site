5f585b8d1228d130caeaf16e6977208212a07bc3
Trueman Phamdo <trueman@phamdo.com>
2024-10-07 22:08:43 -0400

    Refactor to use express

--- server.js
+++ server.js
@@ -1,43 +1,38 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
+const express = require('express');
 
+const app = express();
 const port = 8080;
 const hostname = 'localhost';
 
-const server = http.createServer((req, res) => {
-  let filename;
-  switch (req.url) {
-    case '/':
-      filename = path.join(__dirname, 'index.html');
-      res.statusCode = 200;
-      break;
-    case '/about':
-      filename = path.join(__dirname, 'about.html');
-      res.statusCode = 200;
-      break;
-    case '/contact':
-      filename = path.join(__dirname, 'contact.html');
-      res.statusCode = 200;
-      break;
-    default:
-      filename = './404.html';
-      res.statusCode = 404;
-      break;
-  }
-
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      console.error(`Reading file ${filename} gave ${err}`);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
-    } else {
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data);
-    }
-  });
+app.get('/', (req, res) => {
+  sendFile(res, path.join(__dirname, 'index.html'));
 });
 
-server.listen(port, hostname, () => {
+app.get('/about', (req, res) => {
+  sendFile(res, path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  sendFile(res, path.join(__dirname, 'contact.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('404 - Not Found');
+});
+
+app.listen(port, hostname, () => {
   console.log('Server is running at: ' + hostname + ':' + port);
 });
+
+function sendFile(res, filename) {
+  res.status(200).sendFile(filename, (err) => {
+    if (err) {
+      console.error('Internal Server Error:', err);
+      res.status(err.status).send(`${err.status} error`);
+    } else {
+      console.log(`File ${filename} sent successfully`);
+    }
+  });
+}
